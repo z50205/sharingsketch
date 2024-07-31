eb743952c9793a7744afb0d3b7174117105831fe
z50205 <z50205@yahoo.com.tw>
2024-08-01 00:18:38 +0800

    resize function debug(change othercanvases top and left simultaneously )

--- PSS/__init__.py
+++ PSS/__init__.py
@@ -9,13 +9,13 @@ def create_app():
     app.config.from_object(Config)
     # socketio.init_app(app)
     # socketio.init_app(app, cors_allowed_origins='https://beamviewer.bizara.link')
-    socketio.init_app(app, cors_allowed_origins=['http://52.195.89.113','http://52.195.89.113:8000'])
+    socketio.init_app(app, cors_allowed_origins=['http://52.195.89.113','http://52.195.89.113:8002'])
     db.init_app(app)
     migrate.init_app(app,db)
     login_manager.init_app(app)
     mail.init_app(app)
     app.config["DEBUG"]=True
-    app.config["SECRET_KEY"]="secret"
+    # app.config["SECRET_KEY"]="secret"
     app.add_url_rule('/','index',index,methods=['GET','POST'])
     app.add_url_rule('/register','register',register,methods=['GET','POST'])
     app.add_url_rule('/room','room',room,methods=['GET','POST'])
--- manager.py
+++ manager.py
@@ -10,7 +10,7 @@ from PSS.models.user import User
 app = create_app()
 
 # CORS(app,resources={r"/*": {"origins": "https://beamviewer.bizara.link"}})
-CORS(app,resources={r"/*": {"origins": ["http://52.195.89.113","http://52.195.89.113:8000"]}})
+CORS(app,resources={r"/*": {"origins": ["http://52.195.89.113","http://52.195.89.113:8002"]}})
 # socketio.run(app)
 # migrate = Migrate(app, db)
 
